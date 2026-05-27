e6ee463bfb00fe35a2f2e258b43c6fbb01f4bd21
Andika Rofif Sulistyawan <andikarofifsulistyawan@outlook.com>
2026-05-27 15:35:38 +0700

    menambah logic insertion sort berdasarkan tahun terbit secara ascending serta descending

--- repository.go
+++ repository.go
@@ -64,3 +64,57 @@ func (repository *BukuRepository) GetAllBukuSortByTahunTerbitDescendingSelection
 
 	return hasil
 }
+
+func (repository *BukuRepository) GetAllBukuSortByTahunTerbitAscendingInsertionSort() DaftarBuku {
+	var hasil DaftarBuku
+	var temp Buku
+	var i int
+	var j int
+
+	if repository.TotalBuku == 0 {
+		return DaftarBuku{}
+	}
+
+	hasil = repository.KoleksiBuku
+
+	for i = 1; i < repository.TotalBuku; i++ {
+		temp = hasil[i]
+		j = i
+
+		for j > 0 && temp.TahunTerbit < hasil[j-1].TahunTerbit {
+			hasil[j] = hasil[j-1]
+			j--
+		}
+
+		hasil[j] = temp
+	}
+
+	return hasil
+}
+
+func (repository *BukuRepository) GetAllBukuSortByTahunTerbitDescendingInsertionSort() DaftarBuku {
+	var hasil DaftarBuku
+	var temp Buku
+	var i int
+	var j int
+
+	if repository.TotalBuku == 0 {
+		return DaftarBuku{}
+	}
+
+	hasil = repository.KoleksiBuku
+
+	for i = 1; i < repository.TotalBuku; i++ {
+		temp = hasil[i]
+		j = i
+
+		for j > 0 && temp.TahunTerbit > hasil[j-1].TahunTerbit {
+			hasil[j] = hasil[j-1]
+			j--
+		}
+
+		hasil[j] = temp
+	}
+
+	return hasil
+}
