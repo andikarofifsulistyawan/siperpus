fcdfbd9045a2b46c8ab72896dda34c5ced78f24e
Andika Rofif Sulistyawan <andikarofifsulistyawan@outlook.com>
2026-05-27 16:16:13 +0700

    menambah logic insertion sort berdasarkan id serta judul secara ascending untuk binary search

--- repository.go
+++ repository.go
@@ -1,8 +1,64 @@
 package main
 
+import "strings"
+
 type BukuRepository struct {
 	KoleksiBuku DaftarBuku
 	TotalBuku   int
+}
+
+func (repository *BukuRepository) GetAllBukuSortByIDAscendingInsertionSort() DaftarBuku {
+	var hasil DaftarBuku
+	var temp Buku
+	var i int
+	var j int
+
+	if repository.TotalBuku == 0 {
+		return DaftarBuku{}
+	}
+
+	hasil = repository.KoleksiBuku
+
+	for i = 1; i < repository.TotalBuku; i++ {
+		temp = hasil[i]
+		j = i
+
+		for j > 0 && strings.ToLower(temp.ID) < strings.ToLower(hasil[j-1].ID) {
+			hasil[j] = hasil[j-1]
+			j--
+		}
+
+		hasil[j] = temp
+	}
+
+	return hasil
+}
+
+func (repository *BukuRepository) GetAllBukuSortByJudulAscendingInsertionSort() DaftarBuku {
+	var hasil DaftarBuku
+	var temp Buku
+	var i int
+	var j int
+
+	if repository.TotalBuku == 0 {
+		return DaftarBuku{}
+	}
+
+	hasil = repository.KoleksiBuku
+
+	for i = 1; i < repository.TotalBuku; i++ {
+		temp = hasil[i]
+		j = i
+
+		for j > 0 && strings.ToLower(temp.Judul) < strings.ToLower(hasil[j-1].Judul) {
+			hasil[j] = hasil[j-1]
+			j--
+		}
+
+		hasil[j] = temp
+	}
+
+	return hasil
 }
 
 func (repository *BukuRepository) GetAllBukuSortByTahunTerbitAscendingSelectionSort() DaftarBuku {
